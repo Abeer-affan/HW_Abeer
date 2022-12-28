2c79bae00f0ad9c4f0c43a22cb9543f1b8e3408b
Abeer Affan <abeer.affan1810@gmail.com>
2022-12-28 21:10:28 +0200

    firsf solution

--- hw3/h.w3.py
+++ hw3/h.w3.py
@@ -1,1 +1,18 @@
-print ("hi this my 3th solution ")
+#Abeer Affan
+
+#Ex1
+my_dic={"name,Last_name" :"Abeer Affan", "age":"28", "phone numbe": "0548389626"}
+print(my_dic)
+
+#ex2
+def replace_func(list):
+    if len(list)<4:
+        print( "Error , your list is shorter than 5")
+    else :
+        list[4]="@"
+    return (list)
+print(replace_func(["a","a",5,8,6,5,8,4]))
+print(replace_func(["c",5,2])
+
+#ex3
+
